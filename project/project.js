3de3d71c07beb428417b30076dd7e89c4c58c2fa
cford14 <cford14@gmail.com>
2020-01-23 15:00:46 -0600

    committed new project pagees

---
+++ project/project.js
@@ -0,0 +1,22 @@
+const itemCompiler = (item, quantity, notes, imageFileName, arrayOfLists) => {
+    return {
+        item, //string
+        quantity, //floor
+        notes, //string
+        imageFileName, //file name
+        stores //array
+    }
+};
+
+/*let master = masterList('crackers', 2, '', 'abcd.jpg', ['Walmart', 'Publix']);
+console.log(master);
+
+master.push(masterList('cookies', 3, '', '', ['Winn Dixie', 'Publix', 'Walmart']));
+console.log(master);*/
+
+
+const listMaker (item, quantity, notes, imageFileName, stores) => {
+    masterList = []; //how do I import a saved list here?
+    
+
+}
